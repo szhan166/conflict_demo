aa082fdf1b7cea2d75178f7ac3349545b4a4caea
Shuyue Zhang <szhan166@asu.edu>
2022-01-08 12:55:55 -0800

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,9 @@
-def hello_world():
-    print(f'Hello World!')
+def hello_world(city, state):
+    print(f'Hello World {city}, {state}!')
 
 
 def main():
-    hello_world()
+    hello_world('NYC', 'New York')
 
 
 main()
